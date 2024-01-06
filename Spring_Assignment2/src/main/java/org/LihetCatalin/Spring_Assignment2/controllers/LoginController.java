989c7e70b959c7b09a7fe3df9cb4d9618e85039a
LihetCatalin <catalin.lihet20@gmail.com>
2024-01-06 18:49:06 +0200

    Added simple login interface

--- Spring_Assignment2/src/main/java/org/LihetCatalin/Spring_Assignment2/controllers/LoginController.java
+++ Spring_Assignment2/src/main/java/org/LihetCatalin/Spring_Assignment2/controllers/LoginController.java
@@ -1,4 +1,13 @@
 package org.LihetCatalin.Spring_Assignment2.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+    @GetMapping
+    public String loginPage(){
+        return "login";
+    }
 }
